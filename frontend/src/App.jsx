093aefa5646cd3a14600cc38326ca09c478e32b4
animward <animward@gmail.com>
2024-04-18 14:51:46 -0700

    removed some duplicate code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import PhotoList from './components/PhotoList';
 import './App.scss';
 import TopNavigationBar from './components/TopNavigationBar';
-import FavIcon from 'components/FavIcon';
 
 const App = () => {
   return (
     <div className="App">
       <TopNavigationBar />
       <PhotoList/>
-      <FavIcon isActive={true} />
     </div>
   );
 };
