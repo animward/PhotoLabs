1e03a823837e969527a2690aa5051ca13a3bc661
animward <animward@gmail.com>
2024-04-18 15:08:18 -0700

    added home route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import PhotoList from './components/PhotoList';
+
 import './App.scss';
-import TopNavigationBar from './components/TopNavigationBar';
+
+import HomeRoute from './components/HomeRoute';
 
 const App = () => {
   return (
     <div className="App">
-      <TopNavigationBar />
-      <PhotoList/>
+    <HomeRoute />
     </div>
   );
 };
