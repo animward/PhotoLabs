dc26de6329baaf88571acf12db828e92332b8a3a
animward <animward@gmail.com>
2024-04-18 15:13:28 -0700

    set up with mock data

--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import TopNavigationBar from './TopNavigationBar';
 import PhotoList from './PhotoList';
 import '../styles/HomeRoute.scss';
+import TopicList from './TopicList';
+import photos from 'mocks/photos';
+import topics from 'mocks/topics';
 
 const HomeRoute = () => {
   return (
     <div className="home-route">
       <TopNavigationBar />
-      <PhotoList />
+      <PhotoList photoData={ photos } />
+      <TopicList TopicList={ topics } />
     </div>
   );
 };
