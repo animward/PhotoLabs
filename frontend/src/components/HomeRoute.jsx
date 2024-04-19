955a9af7dd10ea27769e7947fd2547e636a9a547
animward <animward@gmail.com>
2024-04-18 18:19:18 -0700

    toggle favorites

--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -5,12 +5,24 @@ import '../styles/HomeRoute.scss';
 import TopicList from './TopicList';
 import topics from '../mocks/topics';
 import photos from '../mocks/photos';
+import { useState } from 'react';
 
 const HomeRoute = ({ photos }) => {
+
+	const [favoritePhotos, setFavoritePhotos] = useState([]);
+
+  const toggleFavorite = (photoId) => {
+    if (favoritePhotos.includes(photoId)) {
+      setFavoritePhotos(favoritePhotos.filter(id => id !== photoId));
+    } else {
+      setFavoritePhotos([...favoritePhotos, photoId]);
+    }
+  };
+
   return (
     <div className="home-route">
-      <TopNavigationBar />
-      <PhotoList photos={ photos } />
+      <TopNavigationBar favoriteCount={favoritePhotos.length} />
+      <PhotoList photos={ photos } toggleFavorite={toggleFavorite} favoritePhotos={favoritePhotos} />
       <TopicList topics={ topics } />
     </div>
   );
