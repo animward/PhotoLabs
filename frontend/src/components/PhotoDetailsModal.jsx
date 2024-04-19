af79d9853ca92f4a3b5f47256366b387f55f11ee
animward <animward@gmail.com>
2024-04-19 14:54:31 -0700

    log photoData

--- frontend/src/components/PhotoDetailsModal.jsx
+++ frontend/src/components/PhotoDetailsModal.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect } from 'react';
 import '../styles/PhotoDetailsModal.scss';
 
-const PhotoDetailsModal = ({ isOpen, onClose }) => {
+const PhotoDetailsModal = ({ isOpen, onClose, photoData }) => {
 
 	useEffect(() => {
     console.log('isOpen:', isOpen); // Log the value of isOpen when it changes
   }, [isOpen]);
+
+	useEffect(() => {
+    console.log('photoData:', photoData);
+  }, [photoData]);
 
   const handleClose = () => {
     onClose();
