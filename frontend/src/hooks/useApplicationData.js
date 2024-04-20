1fa29ee65c5043cf5c0be126e84d792f8ac3e517
animward <animward@gmail.com>
2024-04-19 18:14:54 -0700

    use reducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,17 +1,43 @@
-import { useState, useEffect } from 'react';
+import { useReducer, useEffect } from 'react';
+
+const ACTIONS = {
+  UPDATE_FAV_PHOTO_IDS: 'UPDATE_FAV_PHOTO_IDS',
+  SET_PHOTO_SELECTED: 'SET_PHOTO_SELECTED',
+  CLOSE_PHOTO_DETAILS_MODAL: 'CLOSE_PHOTO_DETAILS_MODAL'
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTIONS.UPDATE_FAV_PHOTO_IDS:
+      return { ...state, favoritePhotos: action.payload.photoIds };
+    case ACTIONS.SET_PHOTO_SELECTED:
+      return { ...state, selectedPhoto: action.payload.photoId };
+    case ACTIONS.CLOSE_PHOTO_DETAILS_MODAL:
+      return { ...state, displayModal: false };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+};
 
 const useApplicationData = () => {
-  const [state, setState] = useState({
-  });
+  const initialState = {
+    favoritePhotos: [],
+    selectedPhoto: null,
+    displayModal: false
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const updateToFavPhotoIds = (photoIds) => {
-
+    dispatch({ type: ACTIONS.UPDATE_FAV_PHOTO_IDS, payload: { photoIds } });
   };
 
   const setPhotoSelected = (photoId) => {
+    dispatch({ type: ACTIONS.SET_PHOTO_SELECTED, payload: { photoId } });
   };
 
   const onClosePhotoDetailsModal = () => {
+    dispatch({ type: ACTIONS.CLOSE_PHOTO_DETAILS_MODAL });
   };
 
   useEffect(() => {
@@ -21,7 +47,7 @@ const useApplicationData = () => {
     state,
     updateToFavPhotoIds,
     setPhotoSelected,
-    onClosePhotoDetailsModal,
+    onClosePhotoDetailsModal
   };
 };
 
