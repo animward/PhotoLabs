b2e8f6dab370c67153eaff956f7257903a0016e9
animward <animward@gmail.com>
2024-04-19 18:07:05 -0700

    add hook structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,31 @@ import React from 'react';
 
 import './App.scss';
 
-import HomeRoute from './components/HomeRoute';
+import HomeRoute from './components/HomeRoute'; // Add missing import statement
 import photos from './mocks/photos';
 import topics from './mocks/topics';
-import { useState } from 'react';
+import useApplicationData from './hooks/useApplicationData';
 
 const App = () => {
+  const {
+    state,
+    onPhotoSelect,
+    updateToFavPhotoIds,
+    onLoadTopic,
+    onClosePhotoDetailsModal,
+  } = useApplicationData();
 
   return (
     <div className="App">
-    <HomeRoute photos={photos} topics={topics}/>
+      <HomeRoute
+        photos={photos}
+        topics={topics}
+        state={state}
+        onPhotoSelect={onPhotoSelect}
+        updateToFavPhotoIds={updateToFavPhotoIds}
+        onLoadTopic={onLoadTopic}
+        onClosePhotoDetailsModal={onClosePhotoDetailsModal}
+      />
     </div>
   );
 };
---
+++ frontend/src/hooks/useApplicationData.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+
+const useApplicationData = () => {
+  const [state, setState] = useState({
+  });
+
+  const updateToFavPhotoIds = (photoIds) => {
+
+  };
+
+  const setPhotoSelected = (photoId) => {
+  };
+
+  const onClosePhotoDetailsModal = () => {
+  };
+
+  useEffect(() => {
+  }, []);
+
+  return {
+    state,
+    updateToFavPhotoIds,
+    setPhotoSelected,
+    onClosePhotoDetailsModal,
+  };
+};
+
+export default useApplicationData;
